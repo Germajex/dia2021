1c9216c4dc5952bb9b8888ab9973f370ea6c6b79
Jacopo <you@example.com>
2021-05-27 11:32:49 +0200

    Environment stampa il seed alla creazione

--- code/src/Environment.py
+++ code/src/Environment.py
@@ -11,8 +11,10 @@ class Environment:
     def __init__(self, random_seed=None):
         if random_seed is None:
             self.rng: Generator = default_rng()
-        else:
-            self.rng: Generator = default_rng(seed=random_seed)
+            random_seed = self.rng.integers(0, 2**32)
+
+        self.rng: Generator = default_rng(seed=random_seed)
+        print(f'Created environment with seed {random_seed}')
 
         # TODO self.customerClasses = CustomerClasses(self.rng)
         # TODO spostare dentro a CustomerClasses?
