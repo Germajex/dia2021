New confidence bound computation for UCB in mode rwd Fix TS Learner in mode rwd

code/src/bandit/mainbandit.py
@@ -6,12 +6,8 @@ from src.CustomerClassCreator import CustomerClassCreator
 import numpy as np
 import LearningStats as Stats
 
-# seed=0xdeadbeef
-rng = np.random.default_rng()
-creator = CustomerClassCreator()
-
 # Fixed variables for our problem
-N_ROUNDS = 1000
+N_ROUNDS = 365
 prices = np.linspace(10, 100, 10)
 n_arms = len(prices)
 bid = 10
@@ -19,7 +15,7 @@ bid = 10
 print("\n > > Starting simulation... < <\n")
 
 # Create customer and environment
-env = Environment()
+env = Environment(random_seed=None)
 print(f'Generated environment with seed {env.get_seed()}')
 bandit_env = BanditEnvironment(environment=env, n_arms=n_arms)
 
@@ -35,7 +31,7 @@ print(f"And expected cr {optimal_cr:.2f}")
 clairvoyant = bandit_env.get_clairvoyant_cumulative_rewards_price(N_ROUNDS, prices, bid)
 
 # Test algorithms
-tsLearner_rwd = TSLearner(n_arms, 0.5, bandit_env)
+tsLearner_rwd = TSLearner(n_arms, 1, bandit_env)
 tsLearner_cr = TSLearner(n_arms, 0, bandit_env)
 ucbLearner_cr = UCB1Learner(n_arms, bandit_env)
 ucbLearner_rwd = UCB1Learner(n_arms, bandit_env)

code/src/bandit/ts/TSLearner.py
@@ -66,7 +66,10 @@ class TSLearner:
     # This method updated the Beta prior distributions of the price arms. This is the standard update done by a
     # classic TS
     def update_beta_price_priors(self, arm_n, successes, failures):
-        self.beta_price_priors[arm_n].update_params(successes, failures)
+        s = np.ceil(successes/4)
+        f = np.ceil(failures/4)
+
+        self.beta_price_priors[arm_n].update_params(s, f)
 
     # Method used to select the most promising price to be tested. Returns the index of the selected arm
     def choose_arm_price(self, mode):

code/src/bandit/ucb/UCB1Learner.py
@@ -35,7 +35,7 @@ class UCB1Learner:
         elif mode == 'cr':
             self.c = 0.1
         elif mode == 'rwd':
-            self.c = 200
+            self.c = 0.01
         else:
             self.c = 1
 
@@ -126,13 +126,26 @@ class UCB1Learner:
 
     # Implementation of the UCB confidence bound. It needs the arm, the timestamp and an hyper-parameter c
     def ucb_confidence_bound(self, a, t, mode):
-        n_pulls = 0
         if mode == 'cr':
             n_pulls = len(self.cr_per_arm[a])
+            return self.c * np.sqrt(2 * np.log(t) / n_pulls)
         elif mode == 'rwd':
             n_pulls = len(self.rewards_per_arm[a])
+            b = self.get_max_reward()
+            sigma = 1/(t**4)
+            num = self.c*np.log((4*self.n_arms*(t**3))/sigma)
+            den = n_pulls
 
-        return self.c * np.sqrt(2 * np.log(t) / n_pulls)
+            return b*np.sqrt(num/den)
+
+    def get_max_reward(self):
+        max_rwd = 0
+        for r in self.rewards_per_arm:
+            r_max = np.max(r)
+            if r_max > max_rwd:
+                max_rwd = r_max
+
+        return max_rwd
 
     # Pulls the given arm, which is a price to be tested. Also appends the collected reward, or the cr, depending on the
     # operation mode

code/src/utils.py
@@ -21,7 +21,7 @@ class NormalGamma:
 
     def sample_zhu_tan(self):
         tau = np.random.gamma(self.alpha, self.beta)
-        theta = np.random.normal(self.mu, 1 / (self.v*tau))
+        theta = np.random.normal(self.mu, 1 / self.v)
 
         return tau, theta
 