f4fe66d389f1093e1a2c53102c57cd237ff3a0b5
Jacopo <you@example.com>
2021-06-26 14:50:40 +0200

    test repeatability with same seed

---
+++ code/src/tests/test_repeatable.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+
+import numpy as np
+
+from src.Environment import Environment
+from src.TSOptimalPriceLearner import TSOptimalPriceLearner
+from src.algorithms import step1
+from src.bandit.BanditEnvironment import BanditEnvironment
+
+
+class TestRepeatable(TestCase):
+    def test_repeatable(self):
+
+        n_runs = 100
+        n_rounds = 100
+        future_visits_delay = 30
+
+        prices = np.arange(10, 101, 10)
+        bids = np.arange(1, 100)
+
+        env = Environment()
+        seed = env.get_seed()
+
+        opt_price, opt_bid, profit = step1(env, prices, bids)
+        bandit_env = BanditEnvironment(env, prices, opt_bid, future_visits_delay)
+        ts_learner = TSOptimalPriceLearner(bandit_env)
+
+        ts_learner.learn(n_rounds)
+        average_crs = ts_learner.get_average_conversion_rates()
+
+        for i in range(1, n_runs + 1):
+            env = Environment(random_seed=seed)
+
+            opt_price, opt_bid, profit = step1(env, prices, bids)
+            bandit_env = BanditEnvironment(env, prices, opt_bid, future_visits_delay)
+
+            ts_learner = TSOptimalPriceLearner(bandit_env)
+
+            ts_learner.learn(n_rounds)
+
+            av_crs = ts_learner.get_average_conversion_rates()
+
+            if not np.equal(average_crs, av_crs).all():
+                print(f'Failed at run n° {i} with seed {seed}')
+                self.fail()
+
