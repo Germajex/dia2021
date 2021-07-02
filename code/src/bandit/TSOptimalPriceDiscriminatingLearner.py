f6fac9bb97fe09177f770333e6fc7f53a02377a8
Jacopo <you@example.com>
2021-07-02 11:27:55 +0200

    optimize imports

--- code/src/bandit/TSOptimalPriceDiscriminatingLearner.py
+++ code/src/bandit/TSOptimalPriceDiscriminatingLearner.py
@@ -1,7 +1,6 @@
 from src.bandit.BanditEnvironment import BanditEnvironment
 from src.bandit.OptimalPriceDiscriminatingLearner import OptimalPriceDiscriminatingLearner
 from src.bandit.TSContext import TSContext
-from src.bandit.UCBContext import UCBContext
 
 
 class TSOptimalPriceDiscriminatingLearner(OptimalPriceDiscriminatingLearner):
