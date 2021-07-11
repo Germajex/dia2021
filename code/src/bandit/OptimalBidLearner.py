Fix safe arm computation for both joint and bid learners

code/src/bandit/OptimalBidLearner.py
@@ -32,7 +32,8 @@ class OptimalBidLearner:
     def choose_next_arm(self):
         # We hope this mask won't ever be all false
         mask = self.compute_safe_arms()
-        return int(np.argmax(self.compute_projected_profits()[mask]))
+        arms_bid_safe = np.where(mask, self.compute_projected_profits(), 0)
+        return int(np.argmax(arms_bid_safe))
 
     def compute_safe_arms(self):
         means = [np.mean(new_clicks) for new_clicks in self.new_clicks_per_arm]

code/src/bandit/OptimalJointLearner.py
@@ -39,7 +39,7 @@ class OptimalJointLearner:
         median_bid = self.n_arms_bid // 2
         arm_price = np.argmax(self.compute_projected_profits_fixed_bid(median_bid))
         mask = self.compute_safe_arms(arm_price)
-        arms_bid_safe = self.compute_projected_profits_fixed_price(arm_price)[mask]
+        arms_bid_safe = np.where(mask, self.compute_projected_profits_fixed_price(arm_price), 0)
         arm_bid = np.argmax(arms_bid_safe)
         return arm_price, arm_bid
 

code/src/scripts/step6.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 from src.Environment import Environment
+from src.algorithms import step1
 from src.bandit.JointBanditEnvironment import JointBanditEnvironment
 from src.bandit.LearningStats import plot_results
 from src.bandit.UCBOptimalJointLearner import UCBOptimalJointLearner
 
 
 def main():
-    prices = np.arange(10, 101, 10)
-    bids = np.linspace(1, 100, num=10, dtype=np.int64)
+    # [!] Feel free to play with the number of arms [!]
+    prices = np.linspace(10, 100, num=10, dtype=np.int64)
+    bids = np.linspace(1, 40, num=10, dtype=np.int64)
 
-    env = Environment(3374247623)
+    env = Environment()
     print(f'Running with seed {env.get_seed()}')
-    n_rounds = 1400
+    n_rounds = 1500
     future_visits_delay = 30
+
+    opt_price, opt_bid, profit = step1(env, prices, bids)
 
     bandit_env = JointBanditEnvironment(env, prices, bids, future_visits_delay)
     regret_length = n_rounds - future_visits_delay
@@ -23,13 +27,15 @@ def main():
     ucb_learner.learn(n_rounds)
     ucb_cumulative_profits = bandit_env.get_learner_cumulative_profit_not_discriminating(ucb_learner.pulled_arms)
 
+    print(f'Optimal price is {opt_price} and optimal bid is {opt_bid}')
+
     ucb_recap = ucb_learner.get_pulled_arms_recap()
     print(f'Bids ->  | ' + ' '.join(f'{bandit_env.bids[b]:5d}' for b in range(ucb_learner.n_arms_bid)))
     print(f'Prices v | ' + '-'*6*ucb_learner.n_arms_bid)
     for arm_p in range(ucb_learner.n_arms_price):
         print(f'{bandit_env.prices[arm_p]:3d}      | ' + ' '.join(f'{p:5d}' for p in ucb_recap[arm_p]))
 
-    plot_results(["UCB"], [ucb_cumulative_profits], clairvoyant_cumulative_profits, regret_length, smooth=True)
+    plot_results(["UCB"], [ucb_cumulative_profits], clairvoyant_cumulative_profits, regret_length, smooth=False)
 
 
 if __name__ == "__main__":